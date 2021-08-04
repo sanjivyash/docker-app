3d05d44b0f72d85391a8e62bc532a50e6b5369c8
sanjivyash <sanjiv.yash@gmail.com>
2021-08-04 16:56:11 +0530

    docker envs enabled

--- backend/config/constants.py
+++ backend/config/constants.py
@@ -1,27 +1,27 @@
 import os 
 
 ##### DOCKER ENV #########
-# host = os.getenv("HOST")
-# port = 8000
-# debug = True
-
-# user = os.getenv("DBUSER")
-# password = os.getenv("DBPASS")
-# uri = f'mongodb://{user}:{password}@mongodb'
-# dbname = os.getenv("DBNAME")
-
-# secret = os.getenv("JWT_SECRET")
-# expiry = float(os.getenv("EXPIRY_TIME"))
-
-##### LOCAL ENV #########
-host = "127.0.0.1"
+host = os.getenv("HOST")
 port = 8000
 debug = True
 
-user = "root"
-password = "password"
-uri = f'mongodb://{user}:{password}@{host}:27017'
-dbname = "movies"
+user = os.getenv("DBUSER")
+password = os.getenv("DBPASS")
+uri = f'mongodb://{user}:{password}@mongodb'
+dbname = os.getenv("DBNAME")
 
-secret = "secret"
-expiry = 6000
+secret = os.getenv("JWT_SECRET")
+expiry = float(os.getenv("EXPIRY_TIME"))
+
+##### LOCAL ENV #########
+# host = "127.0.0.1"
+# port = 8000
+# debug = True
+
+# user = "root"
+# password = "password"
+# uri = f'mongodb://{user}:{password}@{host}:27017'
+# dbname = "movies"
+
+# secret = "secret"
+# expiry = 6000
