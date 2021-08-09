56009d9f0a3b15be56e236831e7e8d36a755bb32
sanjivyash <sanjiv.yash@gmail.com>
2021-08-09 16:08:51 +0530

    finally a working config file

--- backend/config/constants.py
+++ backend/config/constants.py
@@ -1,14 +1,18 @@
 import os 
 
 ##### DOCKER/LOCAL ENV #########
+containerized = os.getenv("DOCKER_ACTIVE", default="False").lower() in ('true', '1', 't')
+
 host = os.getenv("HOST", default="127.0.0.1")
-port = 8000
-debug = True
+port = int(os.getenv("PORT", default="8000"))
+debug = os.getenv("DEBUG", default="True").lower() in ('true', '1', 't')
 
 user = os.getenv("DBUSER", default="root")
 password = os.getenv("DBPASS", default="password")
-uri = f'mongodb://{user}:{password}@mongodb'
+db = "mongodb" if containerized else f'{host}:27017'
+
+uri = f'mongodb://{user}:{password}@{db}'
 dbname = os.getenv("DBNAME", default="movies")
 
 secret = os.getenv("JWT_SECRET", default="secret")
-expiry = float(os.getenv("EXPIRY_TIME", default=600))
+expiry = float(os.getenv("EXPIRY_TIME", default="600"))
