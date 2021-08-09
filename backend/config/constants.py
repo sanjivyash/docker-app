59cc4a7b7bfcb88281952d04c93d6772d3432c2b
sanjivyash <sanjiv.yash@gmail.com>
2021-08-09 15:26:49 +0530

    an even better config file

--- backend/config/constants.py
+++ backend/config/constants.py
@@ -1,14 +1,14 @@
 import os 
 
 ##### DOCKER/LOCAL ENV #########
-host = os.getenv("HOST") or "127.0.0.1"
+host = os.getenv("HOST", default="127.0.0.1")
 port = 8000
 debug = True
 
-user = os.getenv("DBUSER") or "root"
-password = os.getenv("DBPASS") or "password"
+user = os.getenv("DBUSER", default="root")
+password = os.getenv("DBPASS", default="password")
 uri = f'mongodb://{user}:{password}@mongodb'
-dbname = os.getenv("DBNAME") or "movies"
+dbname = os.getenv("DBNAME", default="movies")
 
-secret = os.getenv("JWT_SECRET") or "secret"
-expiry = float(os.getenv("EXPIRY_TIME") or 600)
+secret = os.getenv("JWT_SECRET", default="secret")
+expiry = float(os.getenv("EXPIRY_TIME", default=600))
